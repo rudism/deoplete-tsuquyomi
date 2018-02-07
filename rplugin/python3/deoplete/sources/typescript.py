f225875db434b642fd2806f6418564b209d341e9
Rudis Muiznieks <rmuiznieks@nextgxdx.com>
2018-02-07 11:19:33 -0600

    added signature info to completions

--- rplugin/python3/deoplete/sources/typescript.py
+++ rplugin/python3/deoplete/sources/typescript.py
@@ -1,4 +1,4 @@
-import json
+import re
 
 from .base import Base
 
@@ -7,7 +7,7 @@ class Source(Base):
         Base.__init__(self, vim)
         self.name = 'typescript'
         self.mark = '[TS]'
-        self.filetypes = ["typescript", "tsx", "typescript.tsx", "javascript", "jsx", "javascript.jsx"]
+        self.filetypes = ["typescript"]
         self.input_pattern = '\.'
         self.is_bytepos = True
         self.rank = 500
@@ -16,9 +16,23 @@ class Source(Base):
         return self.vim.call('tsuquyomi#complete', 1, 0)
 
     def gather_candidates(self, context):
-        def translate_candidate(item):
-            item['word'] = item['name']
-            return item
+        bufpath = context['bufpath']
+        line = context['position'][1]
+        offset = context['position'][2]
 
-        completions = self.vim.call('tsuquyomi#tsClient#tsCompletions', context['bufpath'], context['position'][1], context['position'][2], 0)
-        return list(map(translate_candidate, completions))
+        # get all completions
+        completions = self.vim.call('tsuquyomi#tsClient#tsCompletions', bufpath, line, offset, 0)
+        items = []
+        entries = []
+        for item in completions:
+            candidate = {'word': item['name'], 'kind': item['kind']}
+            items.append(candidate)
+            entries.append(item['name'])
+
+        # attach signatures
+        infos = self.vim.call('tsuquyomi#makeCompleteMenu', bufpath, line, offset, entries)
+        for i in range(len(items)):
+            info = re.sub('^\([^\)]+\) ', '', infos[i])
+            items[i]['info'] = info
+
+        return items
